37bf10e26ca0b3e7e3ccfd3592e760a44d7fe0e0
floy-dev <floy-dev@mail.ru>
2023-03-27 01:05:37 +0300

    lw8

--- algorithm/lw8/main.py
+++ algorithm/lw8/main.py
@@ -2,8 +2,22 @@ import tkinter
 
 
 def main():
-    graph = tkinter.Tk()
-    canvas = tkinter.Canvas(graph, width=100, height=100)
+    window = tkinter.Tk()
+
+    canvas = tkinter.Canvas(window, width=400, height=400)
+    canvas.pack()
+
+    window.title("Польша")
+
+    a = canvas.create_oval(50, 50, 300, 300, fill='lightgray', outline="")
+    canvas.move(a, 25, 0)
+
+    a = canvas.create_rectangle(100, 120, 300, 170, fill='white', outline="")
+    canvas.move(a, 0, 0)
+    a = canvas.create_rectangle(100, 170, 300, 220, fill='red', outline="")
+    canvas.move(a, 0, 0)
+
+    window.mainloop()
 
 
 if __name__ == '__main__':
